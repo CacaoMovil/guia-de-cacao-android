5f3acf6281ec7d5d2bb9faeb6b3dab997438910c
jhon <jhon.chavarria@gmail.com>
2015-03-02 21:49:04 -0600

    Adding anim folder and Api class to comunicate with the backend

---
+++ com.kronoscode.com.android/app/src/main/java/kronos/comkronoscodecomandroid/activity/Api/ApiClient.java
@@ -0,0 +1,42 @@
+package kronos.comkronoscodecomandroid.activity.Api;
+
+import org.json.JSONObject;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.http.GET;
+
+/**
+ * Created by jhon on 2/03/15.
+ * This class is the main class for API connection, we will define all the interfaces here that communicate with the backend
+ */
+public final class ApiClient {
+    // API URL
+    public static final String API_URL = "http://cacao.kronoscode.com/api/v1";
+
+    private static cacaoApiInterface sCacaoApiInterface;
+
+    public static cacaoApiInterface getCacaoApiInterface() {
+        if (sCacaoApiInterface == null) {
+            RestAdapter restAdapter = new RestAdapter.Builder()
+                    .setEndpoint(API_URL)
+                    .setLogLevel(RestAdapter.LogLevel.FULL)
+                    .build();
+
+            sCacaoApiInterface = restAdapter.create(cacaoApiInterface.class);
+        }
+
+        return sCacaoApiInterface;
+    }
+
+    /**
+     * This interface will handle all the events with the API
+     */
+    public interface cacaoApiInterface {
+
+        // Get guides
+        @GET("/guides")
+        void getGuides (Callback <JSONObject> callBack);
+
+    }
+}
---
+++ com.kronoscode.com.android/app/src/main/java/kronos/comkronoscodecomandroid/activity/CacaoActivity.java
@@ -0,0 +1,50 @@
+package kronos.comkronoscodecomandroid.activity;
+
+import android.app.ActionBar;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
+import android.view.Window;
+
+import kronos.comkronoscodecomandroid.R;
+
+/**
+ * Created by jhon on 2/03/2015.
+ */
+public class CacaoActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        }
+        setProgressBarIndeterminateVisibility(false);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+              onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
