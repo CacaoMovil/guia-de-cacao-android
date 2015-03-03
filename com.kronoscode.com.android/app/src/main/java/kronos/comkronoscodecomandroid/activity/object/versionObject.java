e3d02f335d2a1cbb008a03842c3d5d885ca214c7
jhon <jhon.chavarria@gmail.com>
2015-03-02 21:52:47 -0600

    Adding internet permission and version object class

---
+++ com.kronoscode.com.android/app/src/main/java/kronos/comkronoscodecomandroid/activity/object/versionObject.java
@@ -0,0 +1,44 @@
+package kronos.comkronoscodecomandroid.activity.object;
+
+/**
+ * Created by jhon on 2/3/15.
+ */
+public class versionObject {
+
+    private String mName;
+    private String mFile;
+    private String mDate;
+    private String mNumVersion;
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmFile() {
+        return mFile;
+    }
+
+    public void setmFile(String mFile) {
+        this.mFile = mFile;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(String mDate) {
+        this.mDate = mDate;
+    }
+
+    public String getmNumVersion() {
+        return mNumVersion;
+    }
+
+    public void setmNumVersion(String mNumVersion) {
+        this.mNumVersion = mNumVersion;
+    }
+}
