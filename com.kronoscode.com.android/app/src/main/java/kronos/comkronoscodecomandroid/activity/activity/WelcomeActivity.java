38517f6e72ac93eee1474953e4f26b5e203f6086
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2015-04-04 23:41:55 -0600

    ui changes part 1

--- com.kronoscode.com.android/app/src/main/java/kronos/comkronoscodecomandroid/activity/activity/WelcomeActivity.java
+++ com.kronoscode.com.android/app/src/main/java/kronos/comkronoscodecomandroid/activity/activity/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import kronos.comkronoscodecomandroid.R;
@@ -13,6 +14,7 @@ public class WelcomeActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_welcome);
 
         Button btnList = (Button) findViewById(R.id.btn_go_to_list);
