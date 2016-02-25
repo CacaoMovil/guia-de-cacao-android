0e455e8b6a70cf4a3a4af5710166a44cf4761a70
Jonathan Chavarria <jhon.chavarria@gmail.com>
2016-02-25 10:23:32 -0600

    haciendo que retrofit lea desde el preferences

--- com.kronoscode.com.android/app/src/main/java/kronos/comkronoscodecomandroid/activity/Api/ApiModule.java
+++ com.kronoscode.com.android/app/src/main/java/kronos/comkronoscodecomandroid/activity/Api/ApiModule.java
@@ -1,19 +1,25 @@
 package kronos.comkronoscodecomandroid.activity.api;
 
+import javax.inject.Inject;
+
 import dagger.Module;
 import dagger.Provides;
 import kronos.comkronoscodecomandroid.BuildConfig;
 import kronos.comkronoscodecomandroid.activity.constants.Constants;
+import kronos.comkronoscodecomandroid.activity.prefs.PersistentStore;
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
 
 @Module
 public class ApiModule {
 
+    @Inject
+    PersistentStore persistentStore;
+
     @Provides
     public GuidesService provideGuidesService() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Constants.DEFAULT_API_URL)
+                .baseUrl(persistentStore.get(PersistentStore.API_URL, Constants.DEFAULT_API_URL))
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit.create(GuidesService.class);
@@ -22,7 +28,7 @@ public class ApiModule {
     @Provides
     public SettingsService settingsService() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Constants.DEFAULT_API_URL)
+                .baseUrl(persistentStore.get(PersistentStore.API_URL, Constants.DEFAULT_API_URL))
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit.create(SettingsService.class);
