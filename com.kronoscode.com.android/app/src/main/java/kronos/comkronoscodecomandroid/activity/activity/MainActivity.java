b550288d445934804f27f33ba47888dd42c60192
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2016-10-24 16:54:54 -0600

    quitando import que no se usa

--- com.kronoscode.com.android/app/src/main/java/kronos/comkronoscodecomandroid/activity/activity/MainActivity.java
+++ com.kronoscode.com.android/app/src/main/java/kronos/comkronoscodecomandroid/activity/activity/MainActivity.java
@@ -54,7 +54,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
